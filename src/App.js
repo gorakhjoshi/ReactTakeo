53bb74eb9b5b704bb86a68d2f6ada486ae2c5726
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-22 13:26:35 +0545

    useState hook in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div>useState</div>
-  )
-}
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-export default App
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+};
+
+const List = (props) =>
+  props.list.map((item) => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
+
+export default App;
